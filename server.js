a85ffb3914f9f0e3ae0188bf508cf2ec28e2a444
pauldropz <pauldropz@gmail.com>
2022-10-03 22:47:18 +0100

    adjustment:const app

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 
 const express = require("expreess");
 
+const app = express();
+
 
 app.get("/", function(request, response){
     response.send("<h1>Node Nigger</h1>")
 })
-const app = express();
 
 
 app.listen(3000, function (){
     console.log("server started on port 3000")
-} );
+});
