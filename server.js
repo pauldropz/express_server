d0a4228c43399d9713143377678ef3a1292cb3a4
pauldropz <pauldropz@gmail.com>
2022-10-03 21:14:38 +0100

    update

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const express = require("expreess");
 const app = express();
 
 
-app.listen(3000); function() {
+app.listen(3000, function function(){
     console.log("server started on port 3000")
-}
+} );
