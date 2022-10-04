13229abc11a41efffdca3673578290b747731c36
pauldropz <pauldropz@gmail.com>
2022-10-04 21:30:32 +0100

    npm module deleted

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// // Jshint esversion:6
-
-// const fs = require("fs");
-
-
-
-// fs.copyFileSync=("file1.txt, file2.txt")
-
-
-
 const fs = require('node:fs/promises');
-const fs = require('node:fs');
+
+
+fs.copyFileSync("file1.txt", "file2.txt");
