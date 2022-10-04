311a3339c5f4b2360ff5579b3602471c5515a153
pauldropz <pauldropz@gmail.com>
2022-10-04 19:21:32 +0100

    chore: Nodev-18 file system

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-// Jshint esversion:6
+// // Jshint esversion:6
 
-const fs = require("fs");
-
-
-
-fs.copyFileSync=("file1.txt, file2.txt")
+// const fs = require("fs");
 
 
+
+// fs.copyFileSync=("file1.txt, file2.txt")
+
+
+
+const fs = require('node:fs/promises');
+const fs = require('node:fs');
