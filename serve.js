3eb89e74dd36f98aedd8551e061064e9078d15a5
pauldropz <pauldropz@gmail.com>
2022-10-07 18:42:17 +0100

    chore: terminal renewal

--- serve.js
+++ serve.js
@@ -1,7 +1,23 @@
+// const mission = "learn";
+
+// if (mission === "learn") {
+//     console.log ("Time to write some node code");
+// } else {
+//     console.log ("Is " + mission + " Actually " + "more " + "Fun" + "!" + "!");
+//     console.log (`Now or never is the best ${mission}`);
+// }
+
+
+
+
+
+
+
 const mission = "learn";
+learn = "Fucking time to code";
 
 if (mission === "learn") {
     console.log ("Time to write some node code");
 } else {
-    console.log ("Is " + mission + " Actually " + "more " + "Fun" + "!" + "!");
+     console.log (`Now or never is the best ${learn}`);
 }
